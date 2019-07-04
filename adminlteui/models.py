0ac0bd93024982f6aff9f3868cdf673a422eacd4
伍岳 <wuyue92tree@163.com>
2019-07-04 17:21:25 +0800

    add menu model

--- adminlteui/models.py
+++ adminlteui/models.py
@@ -2,6 +2,9 @@
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import Permission, Group
+
+from treebeard.mp_tree import MP_Node
 
 
 class Options(models.Model):
@@ -21,3 +24,27 @@ class Options(models.Model):
     class Meta:
         verbose_name = _('Options')
         verbose_name_plural = _('All Options')
+
+
+class Menu(MP_Node):
+    name = models.CharField(max_length=255, verbose_name=_('name'))
+    position = models.CharField(
+        max_length=255, default='left', verbose_name=_('Menu Position'))
+    link = models.CharField(max_length=255, blank=True, null=True,
+                            verbose_name=_('Link'))
+    icon = models.CharField(max_length=255,
+                            blank=True,
+                            null=True,
+                            verbose_name=_('Icon'))
+    permission = models.ManyToManyField(Permission, blank=True,
+                                        verbose_name=_('Permission'))
+
+    permission_group = models.ManyToManyField(Group, blank=True,
+                                              verbose_name=_(
+                                                  'Permission Group'))
+
+    valid = models.BooleanField(default=True, verbose_name=_('Valid'))
+    node_order_by = ['name', 'position']
+
+    def __str__(self):
+        return '{}'.format(self.name)
