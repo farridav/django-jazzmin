52d09af6bacde818d02fdeaadbb4af178ddd1434
伍岳 <wuyue92tree@163.com>
2019-07-08 21:46:17 +0800

    update custom menu display

--- adminlteui/templatetags/adminlte_menu.py
+++ adminlteui/templatetags/adminlte_menu.py
@@ -5,6 +5,8 @@ from django import template
 from django.contrib.admin import AdminSite
 from django.http import HttpRequest
 from django.utils.translation import gettext_lazy as _
+from adminlteui.templatetags.adminlte_options import get_adminlte_option
+from adminlteui.models import Menu
 
 try:
     from django.urls import reverse, resolve
@@ -17,6 +19,53 @@ if django.VERSION < (1, 9):
     simple_tag = register.assignment_tag
 else:
     simple_tag = register.simple_tag
+
+
+def get_reverse_link(link):
+    if not link or '/' in link:
+        return link
+
+    try:
+        return reverse(link)
+    except Exception as e:
+        return None
+
+def get_custom_menu(available_apps):
+    new_available_apps = []
+    menu = Menu.dump_bulk()
+    for menu_item in menu:
+        new_available_apps_item = {}
+        data = (menu_item.get('data'))
+        if data.get('valid') is False:
+            continue
+
+        new_available_apps_item['name'] = data.get('name')
+        new_available_apps_item['icon'] = data.get('icon')
+
+        if data.get('link_type') in (0, 1):
+            new_available_apps_item['admin_url'] = get_reverse_link(
+                data.get('link'))
+
+        children = (menu_item.get('children'))
+        if not children:
+            new_available_apps.append(new_available_apps_item)
+            continue
+        new_available_apps_item['models'] = []
+
+        for children_item in children:
+            if children_item.get('data').get('valid') is False:
+                continue
+            new_children_item = {}
+            new_children_item['name'] = children_item.get('data').get('name')
+            new_children_item['admin_url'] = get_reverse_link(
+                children_item.get('data').get('link')
+            )
+            new_children_item['icon'] = children_item.get('data').get('icon')
+            # new_children_item['admin_url'] = children_item.get('link')
+            new_available_apps_item['models'].append(new_children_item)
+        new_available_apps.append(new_available_apps_item)
+
+    return new_available_apps
 
 
 @simple_tag(takes_context=True)
@@ -43,7 +92,12 @@ def get_menu(context, request):
             except Exception:
                 pass
     if not available_apps:
-        logging.warn('Django Suit was unable to retrieve apps list for menu.')
+        logging.warn('adminlteui was unable to retrieve apps list for menu.')
+
+    use_custom_menu = get_adminlte_option('USE_CUSTOM_MENU')
+    if use_custom_menu.get('USE_CUSTOM_MENU',
+                           '0') == '1' and use_custom_menu.get('valid') is True:
+        return get_custom_menu(available_apps)
 
     for app in available_apps:
         if app.get('app_label') == 'django_admin_settings':
@@ -58,7 +112,8 @@ def get_menu(context, request):
                                                  'delete': True,
                                                  'view': True
                                              },
-                                         'admin_url': '/admin/django_admin_settings/options/general_option/',
+                                         'admin_url': reverse(
+                                             'admin:general_option'),
                                          'view_only': False
                                      }
                                      )
