19dc0fd498f2e433a86077e37c8f5c04cbfe1c07
David Farrington <info@davidfarrington.co.uk>
2020-05-05 13:02:59 +0100

    Tidy tests

--- tests/test_adminlte.py
+++ tests/test_adminlte.py
@@ -7,7 +7,7 @@ User = get_user_model()
 
 class AdminlteTestCase(TestCase):
 
-    def setUp(self) -> None:
+    def setUp(self):
         super().setUp()
         self.user = User.objects.create(
             email='test@test.com', password='test',
@@ -15,17 +15,10 @@ class AdminlteTestCase(TestCase):
         )
         self.client.force_login(self.user)
 
-    def test_smoke(self):
-        """
-        We can render the admin out with nothing broken
-        """
-        url = reverse('admin:index')
-
-        response = self.client.get(url)
-
-        self.assertEqual(response.status_code, 200)
-
     def test_options_admin(self):
+        """
+        The Options admin renders out
+        """
         url = reverse('admin:index')
 
         response = self.client.get(url)
@@ -33,6 +26,9 @@ class AdminlteTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_menu_admin(self):
+        """
+        The menu admin renders out
+        """
         url = reverse('admin:general_option')
 
         response = self.client.get(url)
@@ -41,43 +37,30 @@ class AdminlteTestCase(TestCase):
 
     def test_update_options(self):
         """
-
-        /admin/django_admin_settings/options/   django.contrib.admin.options.changelist_view    admin:django_admin_settings_options_changelist
-        /admin/django_admin_settings/options/<path:object_id>/  django.views.generic.base.RedirectView
-        /admin/django_admin_settings/options/<path:object_id>/change/   django.contrib.admin.options.change_view        admin:django_admin_settings_options_change
-        /admin/django_admin_settings/options/<path:object_id>/delete/   django.contrib.admin.options.delete_view        admin:django_admin_settings_options_delete
-        /admin/django_admin_settings/options/<path:object_id>/history/  django.contrib.admin.options.history_view       admin:django_admin_settings_options_history
-        /admin/django_admin_settings/options/add/       django.contrib.admin.options.add_view   admin:django_admin_settings_options_add
-        /admin/django_admin_settings/options/autocomplete/      django.contrib.admin.options.autocomplete_view  admin:django_admin_settings_options_autocomplete
-        /admin/django_admin_settings/options/general_option/    adminlteui.admin.general_option_view    admin:general_option
-
+        We can update all of the options available, and they affect the UI as expected
         """
-        url = reverse('admin:general_option')
-
-        response = self.client.get(url)
-
-        self.assertEqual(response.status_code, 200)
+        pass
 
     def test_update_site_logo(self):
-        url = reverse('admin:general_option')
-
-        response = self.client.get(url)
-
-        self.assertEqual(response.status_code, 200)
+        """
+        We can add a site logo, and it renders out
+        """
+        pass
 
     def test_exchange_menu(self):
-        url = reverse('admin:general_option')
-
-        response = self.client.get(url)
-
-        self.assertEqual(response.status_code, 200)
+        """
+        We can exchange our basic menu for a custom one, and back
+        """
+        pass
 
     def test_build_custom_menu(self):
-        url = reverse('admin:exchange_menu')
+        """
+        We can build a custom menu and it renders out on top and left side
+        """
+        pass
 
-        response = self.client.get(url)
-
-        self.assertEqual(response.status_code, 200)
-
-    def test_templates_used(self):
+    def test_numqueries(self):
+        """
+        We make a minimal no. of queries on each admin page view
+        """
         pass
---
+++ tests/test_smoke.py
@@ -0,0 +1,37 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+User = get_user_model()
+
+
+class SmokeTestCase(TestCase):
+    """
+    Smoke test rendering of various admin views with adminlte app installed
+    """
+
+    def setUp(self):
+        super().setUp()
+        self.user = User.objects.create(
+            email='test@test.com', password='test',
+            is_staff=True, is_superuser=True, is_active=True
+        )
+
+    def test_login(self):
+        url = reverse('admin:index')
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_dashboard(self):
+        pass
+
+    def test_list_view(self):
+        pass
+
+    def test_detail_view(self):
+        pass
+
+    def test_history_view(self):
+        pass
