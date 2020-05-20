24e21f66d7502a58891b88f40777a2f374d4c16a
David Farrington <info@davidfarrington.co.uk>
2020-05-20 20:32:19 +0100

    Fix tests

--- tests/test_app/settings.py
+++ tests/test_app/settings.py
@@ -80,7 +80,10 @@ TEMPLATES = [{
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.getenv('DB_NAME', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST': {
+            'NAME': ':memory:'
+        }
     }
 }
 
