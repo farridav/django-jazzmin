9a4999070e8d5bebbc50dcd0ab63c053eb9fd57a
David Farrington <info@davidfarrington.co.uk>
2020-05-22 20:34:14 +0100

    Fix broken cookie for menu

--- jazzmin/static/jazzmin/js/main.js
+++ jazzmin/static/jazzmin/js/main.js
@@ -1,7 +1,7 @@
 function setCookie(key, value) {
     var expires = new Date();
     expires.setTime(expires.getTime() + (value * 24 * 60 * 60 * 1000));
-    document.cookie = key + '=' + value + ';expires=' + expires.toUTCString() + '; SameSite=Strict';
+    document.cookie = key + '=' + value + ';expires=' + expires.toUTCString() + '; SameSite=Strict;path=/';
 }
 
 function getCookie(key) {
@@ -9,12 +9,17 @@ function getCookie(key) {
     return keyValue ? keyValue[2] : null;
 }
 
-$(document).ready(function() {
+function eraseCookie(key) {
+    var keyValue = getCookie(key);
+    setCookie(key, keyValue, '-1');
+}
+
+$(document).ready(function () {
     $('a[href="' + window.location.pathname + '"]').addClass('active');
 
     $('.actions select').addClass('form-control');
-
-    $('[data-widget=pushmenu]').bind('click', function() {
+    
+    $('[data-widget=pushmenu]').bind('click', function () {
         var menuClosed = getCookie('jazzy_menu') === 'closed';
         if (!menuClosed) {
             setCookie('jazzy_menu', 'closed');
