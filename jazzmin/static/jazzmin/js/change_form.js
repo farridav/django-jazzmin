cce5f70481b88f17167d339401d20760239d7866
Tirso Rodriguez <tirso.rod@gmail.com>
2020-05-24 17:45:01 +0100

    Add change_form js file

---
+++ jazzmin/static/jazzmin/js/change_form.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.vDateField').addClass('form-control');
+
+    <div class="input-group-prepend">
+                      <span class="input-group-text">
+                        <i class="far fa-calendar-alt"></i>
+                      </span>
+                    </div>
+});
