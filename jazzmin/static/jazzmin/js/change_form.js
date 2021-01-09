8aca12fdfa169db11aa3932c68d80bf779917687
David Farrington <info@davidfarrington.co.uk>
2021-01-09 13:22:46 +0000

    Revert select2 width to auto

--- jazzmin/static/jazzmin/js/change_form.js
+++ jazzmin/static/jazzmin/js/change_form.js
@@ -109,7 +109,7 @@
         // Apply select2 to any select boxes that don't yet have it
         // and are not part of the django's empty-form inline
         const noSelect2 = '.empty-form select, .select2-hidden-accessible, .selectfilter, .selector-available select, .selector-chosen select';
-        $('select').not(noSelect2).select2({ width: 'element' });
+        $('select').not(noSelect2).select2({ width: 'auto' });
     }
 
     $(document).ready(function () {
