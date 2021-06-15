94defc6f6c6af084473b56c77d9d698c2c4fa74c
David Farrington <info@davidfarrington.co.uk>
2021-06-15 09:07:04 +0100

    Demonstrate template overiding

--- tests/test_app/library/settings.py
+++ tests/test_app/library/settings.py
@@ -57,7 +57,9 @@ LOGGING = {
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            os.path.join(BASE_DIR, "templates")
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
