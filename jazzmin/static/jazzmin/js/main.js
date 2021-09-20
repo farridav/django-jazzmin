251bb2bb84a242cfba84e31e66d0f46fc4e0ad2c
saeedhmt <saeed.hemati10894@gmail.com>
2021-09-20 14:21:45 +0430

    when navigation_expanded is false, an app in sidebar would be expanded when a model of the app is selected.

--- jazzmin/static/jazzmin/js/main.js
+++ jazzmin/static/jazzmin/js/main.js
@@ -56,7 +56,14 @@
         const $changeListTable = $('#changelist .results table');
         if ($changeListTable.length && !$changeListTable.hasClass('table table-striped')) {
             $changeListTable.addClass('table table-striped');
-        }
+        };
+
+        var a_active = $('a.nav-link.active');
+        var main_li_parent = a_active.closest('li.nav-item.has-treeview');
+        var ul_child = main_li_parent.children('ul');
+
+        ul_child.show();
+        main_li_parent.addClass('menu-is-opening menu-open');
 
     });
 
