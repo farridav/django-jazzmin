502e9c66e4383401559de9c1449efe38f99bc2b7
Timo Ludwig <ti.ludwig@web.de>
2022-03-26 21:43:42 +0100

    Add templates for password reset
    Fixes #388

--- tests/test_app/library/urls.py
+++ tests/test_app/library/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin, messages
+from django.contrib.auth import views as auth_views
 from django.http import HttpResponseRedirect
 from django.urls import path, re_path, reverse
 from django.views.generic import RedirectView
@@ -30,7 +31,29 @@ urlpatterns = [
     path("i18n/", include("django.conf.urls.i18n")),
 ]
 
-urlpatterns += i18n_patterns(path("admin/", admin.site.urls))
+urlpatterns += i18n_patterns(
+    path(
+        "admin/password_reset/",
+        auth_views.PasswordResetView.as_view(),
+        name="admin_password_reset",
+    ),
+    path(
+        "admin/password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
+    path("admin/", admin.site.urls),
+)
 
 if settings.DEBUG:
     urlpatterns.append(
