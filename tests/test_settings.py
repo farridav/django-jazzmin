2d4a30a6712ae318d39ef0d7b7cdeeb8788857fe
jrodrigueztri <jrodriguez@trialing.org>
2022-10-11 15:02:52 +0200

    Added multiple search_model setting option

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -3,8 +3,8 @@ from jazzmin.settings import get_search_model_string
 
 def test_get_search_model_string():
     # model name is always lower case
-    assert get_search_model_string({"search_model": "books.Book"}) == "books.book"
-    assert get_search_model_string({"search_model": "books.book"}) == "books.book"
+    assert get_search_model_string("books.Book") == "books.book"
+    assert get_search_model_string("books.book") == "books.book"
     # the app name gets never touched
-    assert get_search_model_string({"search_model": "Books.Book"}) == "Books.book"
-    assert get_search_model_string({"search_model": "BookShelf.book"}) == "BookShelf.book"
+    assert get_search_model_string("Books.Book") == "Books.book"
+    assert get_search_model_string("BookShelf.book") == "BookShelf.book"
