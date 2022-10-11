0a771b6ea04dcc38766d263c037259d6b1039b61
Felipe Araujo <345.souza@gmail.com>
2022-10-11 19:05:15 -0300

    Fix modal window not closing on save django >= 4.0

--- jazzmin/static/admin/js/popup_response.js
+++ jazzmin/static/admin/js/popup_response.js
@@ -1,9 +1,11 @@
 (function() {
+
     'use strict';
 
-    let windowRef = window;
-    let windowName, widgetName;
-    let openerRef = windowRef.opener;
+    var windowRef = window;
+    var windowRefProxy;
+    var windowName, widgetName;
+    var openerRef = windowRef.opener;
     if (!openerRef) {
         // related modal is active
         openerRef = windowRef.parent;
@@ -13,44 +15,36 @@
             // django < 3.1 compatibility
             widgetName = openerRef.id_to_windowname(widgetName);
         }
-        windowRef = {
+        windowRefProxy = {
             name: widgetName,
+            location: windowRef.location,
             close: function() {
                 openerRef.dismissRelatedObjectModal();
             }
         };
-    }
-
-    // select before last iframe content window if exists else select openerRef
-    let openerRef2;
-    var iframeHTMLCollection = openerRef.document.getElementsByClassName('related-iframe');
-    if (iframeHTMLCollection.length >= 2) {
-        var beforeLastIframeIndex = iframeHTMLCollection.length - 2;
-        openerRef2 = iframeHTMLCollection[beforeLastIframeIndex].contentWindow;
-    } else {
-        openerRef2 = openerRef;
+        windowRef = windowRefProxy;
     }
 
     // default django popup_response.js
-    const initData = JSON.parse(document.getElementById('django-admin-popup-response-constants').dataset.popupResponse);
+    var initData = JSON.parse(document.getElementById('django-admin-popup-response-constants').dataset.popupResponse);
     switch (initData.action) {
         case 'change':
             if (typeof(openerRef.dismissChangeRelatedObjectPopup) === 'function') {
-                openerRef2.dismissChangeRelatedObjectPopup(windowRef, initData.value, initData.obj, initData.new_value);
+                openerRef.dismissChangeRelatedObjectPopup(windowRef, initData.value, initData.obj, initData.new_value);
             }
             break;
         case 'delete':
             if (typeof(openerRef.dismissDeleteRelatedObjectPopup) === 'function') {
-                openerRef2.dismissDeleteRelatedObjectPopup(windowRef, initData.value);
+                openerRef.dismissDeleteRelatedObjectPopup(windowRef, initData.value);
             }
             break;
         default:
             if (typeof(openerRef.dismissAddRelatedObjectPopup) === 'function') {
-                openerRef2.dismissAddRelatedObjectPopup(windowRef, initData.value, initData.obj);
+                openerRef.dismissAddRelatedObjectPopup(windowRef, initData.value, initData.obj);
             }
             else if (typeof(openerRef.dismissAddAnotherPopup) === 'function') {
                 // django 1.7 compatibility
-                openerRef2.dismissAddAnotherPopup(windowRef, initData.value, initData.obj);
+                openerRef.dismissAddAnotherPopup(windowRef, initData.value, initData.obj);
             }
             break;
     }
