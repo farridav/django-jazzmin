37c795fd565879db44c96605dae77a9355b6ad81
DiablosWhisper <shelyakingleb@gmail.com>
2023-03-24 16:04:19 +0200

    update: fix styles

--- jazzmin/static/jazzmin/js/change_select2.js
+++ jazzmin/static/jazzmin/js/change_select2.js
@@ -1,11 +1,9 @@
 window.onload = function() {
-    selects = document.getElementsByClassName(
-        "select2 select2-container select2-container--admin-autocomplete"
-    )
-    for (const select of selects) {
-        select.setAttribute(
-            "class",
+    document.querySelectorAll(
+        ".select2.select2-container.select2-container--admin-autocomplete"
+    ).forEach(select => select.setAttribute(
+            "class", 
             "select2 select2-container select2-container--default select2-container--focus"
         )
-    }
+    )
 }
