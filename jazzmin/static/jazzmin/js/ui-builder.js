582bdbdd4f0c3cec63e98c0a14e2091af1753075
DiablosWhisper <shelyakingleb@gmail.com>
2023-03-24 16:09:44 +0200

    ...

--- jazzmin/static/jazzmin/js/ui-builder.js
+++ jazzmin/static/jazzmin/js/ui-builder.js
@@ -341,3 +341,14 @@
     }
 
 })(jQuery);
+
+
+window.onload = function() {
+    document.querySelectorAll(
+        ".select2.select2-container.select2-container--admin-autocomplete"
+    ).forEach(select => select.setAttribute(
+            "class", 
+            "select2 select2-container select2-container--default select2-container--focus"
+        )
+    )
+}
