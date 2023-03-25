0a3b3f531cee033539562f3789cfcf2a9d114305
DiablosWhisper <shelyakingleb@gmail.com>
2023-03-25 18:19:54 +0200

    update: width of select2

--- jazzmin/static/jazzmin/js/change_select2.js
+++ jazzmin/static/jazzmin/js/change_select2.js
@@ -1,9 +1,15 @@
 window.onload = function() {
     document.querySelectorAll(
         ".select2.select2-container.select2-container--admin-autocomplete"
-    ).forEach(select => select.setAttribute(
-            "class", 
-            "select2 select2-container select2-container--default select2-container--focus"
-        )
+    ).forEach((select) => {
+            select.setAttribute(
+                "class", 
+                "select2 select2-container select2-container--default select2-container--focus"
+            )
+            select.setAttribute(
+                "style",
+                "width: 100%"
+            )
+        }
     )
 }
