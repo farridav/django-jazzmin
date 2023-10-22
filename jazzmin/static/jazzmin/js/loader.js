4a4e2371c10572637f10a0d6168a3d86c1a6bdaa
DiablosWhisper <shelyakingleb@gmail.com>
2023-10-22 12:23:35 +0300

    update: added loader to menu buttons click to reduce freezing time

--- jazzmin/static/jazzmin/js/loader.js
+++ jazzmin/static/jazzmin/js/loader.js
@@ -1,3 +1,11 @@
 window.addEventListener('load', function(){
-    document.querySelector('body').classList.add("loaded")  
+    document.querySelector('body').classList.add("loaded")
+    
+    const navItems = document.querySelectorAll('ul.nav-sidebar li.nav-item');
+
+    navItems.forEach(item => {
+        item.addEventListener('click', function() {
+            document.querySelector('body').classList.remove("loaded")  
+        });
+    });
 });
