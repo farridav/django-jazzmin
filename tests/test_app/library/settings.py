029e7bf6198fe4039f0e7b9df99cbf9e2bcc53a3
David Farrington <david@shipit.ltd>
2024-03-23 09:59:54 +0000

    Linting and project status update

--- tests/test_app/library/settings.py
+++ tests/test_app/library/settings.py
@@ -71,9 +71,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        "TEST": {
-            "NAME": ":memory:"
-        }
+        "TEST": {"NAME": ":memory:"},
     },
 }
 
