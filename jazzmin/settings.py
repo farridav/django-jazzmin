50ae0681d93bdad0ffeecff303bb9703463ee112
David Farrington <david@shipit.ltd>
2024-10-26 22:15:40 +0100

    working through tweaks and settings classes

--- jazzmin/settings.py
+++ jazzmin/settings.py
@@ -1,232 +1,34 @@
-import copy
 import logging
-from typing import Any, Dict
+from typing import Any, Union
 
 from django.conf import settings
 from django.templatetags.static import static
 
-from jazzmin.types import DarkThemes
+from jazzmin.types import JazzminSettings
 
-from .utils import get_admin_url, get_model_meta
+from .types import DarkThemes, UITweaks
 
 logger = logging.getLogger(__name__)
 
-DEFAULT_SETTINGS: Dict[str, Any] = {
-    # title of the window (Will default to current_admin_site.site_title)
-    "site_title": None,
-    # Title on the login screen (19 chars max) (will default to current_admin_site.site_header)
-    "site_header": None,
-    # Title on the brand (19 chars max) (will default to current_admin_site.site_header)
-    "site_brand": None,
-    # Relative path to logo for your site, used for brand on top left (must be present in static files)
-    "site_logo": "vendor/adminlte/img/AdminLTELogo.png",
-    # Relative path to logo for your site, used for login logo (must be present in static files. Defaults to site_logo)
-    "login_logo": None,
-    # Logo to use for login form in dark themes (must be present in static files. Defaults to login_logo)
-    "login_logo_dark": None,
-    # CSS classes that are applied to the logo
-    "site_logo_classes": "img-circle",
-    # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
-    "site_icon": None,
-    # Welcome text on the login screen
-    "welcome_sign": "Welcome",
-    # Copyright on the footer
-    "copyright": "",
-    # The model admin to search from the search bar, search bar omitted if excluded
-    "search_model": None,
-    # Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
-    "user_avatar": None,
-    ############
-    # Top Menu #
-    ############
-    # Links to put along the nav bar
-    "topmenu_links": [],
-    #############
-    # User Menu #
-    #############
-    # Additional links to include in the user menu on the top right ('app' url type is not allowed)
-    "usermenu_links": [],
-    #############
-    # Side Menu #
-    #############
-    # Whether to display the side menu
-    "show_sidebar": True,
-    # Whether to aut expand the menu
-    "navigation_expanded": True,
-    # Hide these apps when generating side menu e.g (auth)
-    "hide_apps": [],
-    # Hide these models when generating side menu (e.g auth.user)
-    "hide_models": [],
-    # List of apps to base side menu ordering off of
-    "order_with_respect_to": [],
-    # Custom links to append to side menu app groups, keyed on lower case app label
-    # or makes a new group if the given app label doesnt exist in installed apps
-    "custom_links": {},
-    # Custom icons for side menu apps/models See the link below
-    # https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,
-    # 5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,
-    # 5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
-    # for the full list of 5.13.0 free icon classes
-    "icons": {"auth": "fas fa-users-cog", "auth.user": "fas fa-user", "auth.Group": "fas fa-users"},
-    # Icons that are used when one is not manually specified
-    "default_icon_parents": "fas fa-chevron-circle-right",
-    "default_icon_children": "fas fa-circle",
-    #################
-    # Related Modal #
-    #################
-    # Activate Bootstrap modal
-    "related_modal_active": False,
-    #############
-    # UI Tweaks #
-    #############
-    # Relative paths to custom CSS/JS scripts (must be present in static files)
-    "custom_css": None,
-    "custom_js": None,
-    # Whether to link font from fonts.googleapis.com (use custom_css to supply font otherwise)
-    "use_google_fonts_cdn": True,
-    # Whether to show the UI customizer on the sidebar
-    "show_ui_builder": False,
-    ###############
-    # Change view #
-    ###############
-    # Render out the change view as a single form, or in tabs, current options are
-    # - single
-    # - horizontal_tabs (default)
-    # - vertical_tabs
-    # - collapsible
-    # - carousel
-    "changeform_format": "horizontal_tabs",
-    # override change forms on a per modeladmin basis
-    "changeform_format_overrides": {},
-    # Add a language dropdown into the admin
-    "language_chooser": False,
-}
 
-#######################################
-# Currently available UI tweaks       #
-# Use the UI builder to generate this #
-#######################################
+def get_settings() -> JazzminSettings:
+    jazzmin_settings: Union[JazzminSettings, dict[str, Any], None] = getattr(settings, "JAZZMIN_SETTINGS")
+    if isinstance(jazzmin_settings, dict):
+        jazzmin_settings = JazzminSettings(**jazzmin_settings)
 
-DEFAULT_UI_TWEAKS: Dict[str, Any] = {
-    # Small text on the top navbar
-    "navbar_small_text": False,
-    # Small text on the footer
-    "footer_small_text": False,
-    # Small text everywhere
-    "body_small_text": False,
-    # Small text on the brand/logo
-    "brand_small_text": False,
-    # brand/logo background colour
-    "brand_colour": False,
-    # Link colour
-    "accent": "accent-primary",
-    # topmenu colour
-    "navbar": "navbar-white navbar-light",
-    # topmenu border
-    "no_navbar_border": False,
-    # Make the top navbar sticky, keeping it in view as you scroll
-    "navbar_fixed": False,
-    # Whether to constrain the page to a box (leaving big margins at the side)
-    "layout_boxed": False,
-    # Make the footer sticky, keeping it in view all the time
-    "footer_fixed": False,
-    # Make the sidebar sticky, keeping it in view as you scroll
-    "sidebar_fixed": False,
-    # sidemenu colour
-    "sidebar": "sidebar-dark-primary",
-    # sidemenu small text
-    "sidebar_nav_small_text": False,
-    # Disable expanding on hover of collapsed sidebar
-    "sidebar_disable_expand": False,
-    # Indent child menu items on sidebar
-    "sidebar_nav_child_indent": False,
-    # Use a compact sidebar
-    "sidebar_nav_compact_style": False,
-    # Use the AdminLTE2 style sidebar
-    "sidebar_nav_legacy_style": False,
-    # Use a flat style sidebar
-    "sidebar_nav_flat_style": False,
-    # Bootstrap theme to use (default, or from bootswatch, see THEMES below)
-    "theme": "default",
-    # Theme to use instead if the user has opted for dark mode (e.g darkly/cyborg/slate/solar/superhero)
-    "dark_mode_theme": None,
-    # The classes/styles to use with buttons
-    "button_classes": {
-        "primary": "btn-primary",
-        "secondary": "btn-secondary",
-        "info": "btn-info",
-        "warning": "btn-warning",
-        "danger": "btn-danger",
-        "success": "btn-success",
-    },
-}
-
-def get_search_model_string(search_model: str) -> str:
-    """
-    Get a search model string for reversing an admin url.
-
-    Ensure the model name is lower cased but remain the app name untouched.
-    """
-
-    app, model_name = search_model.split(".")
-    return "{app}.{model_name}".format(app=app, model_name=model_name.lower())
-
-
-def get_settings() -> Dict:
-    jazzmin_settings = copy.deepcopy(DEFAULT_SETTINGS)
-    user_settings = {x: y for x, y in getattr(settings, "JAZZMIN_SETTINGS", {}).items() if y is not None}
-    jazzmin_settings.update(user_settings)
-
-    # Extract search model configuration from search_model setting
-    if jazzmin_settings["search_model"]:
-        if not isinstance(jazzmin_settings["search_model"], list):
-            jazzmin_settings["search_model"] = [jazzmin_settings["search_model"]]
-
-        jazzmin_settings["search_models_parsed"] = []
-        for search_model in jazzmin_settings["search_model"]:
-            jazzmin_search_model = {}
-            jazzmin_search_model["search_url"] = get_admin_url(get_search_model_string(search_model))
-
-            model_meta = get_model_meta(search_model)
-            if model_meta:
-                jazzmin_search_model["search_name"] = model_meta.verbose_name_plural.title()
-            else:
-                jazzmin_search_model["search_name"] = search_model.split(".")[-1] + "s"
-            jazzmin_settings["search_models_parsed"].append(jazzmin_search_model)
-
-    # Deal with single strings in hide_apps/hide_models and make sure we lower case 'em
-    if isinstance(jazzmin_settings["hide_apps"], str):
-        jazzmin_settings["hide_apps"] = [jazzmin_settings["hide_apps"]]
-    jazzmin_settings["hide_apps"] = [x.lower() for x in jazzmin_settings["hide_apps"]]
-
-    if isinstance(jazzmin_settings["hide_models"], str):
-        jazzmin_settings["hide_models"] = [jazzmin_settings["hide_models"]]
-    jazzmin_settings["hide_models"] = [x.lower() for x in jazzmin_settings["hide_models"]]
-
-    # Ensure icon model names and classes are lower case
-    jazzmin_settings["icons"] = {x.lower(): y.lower() for x, y in jazzmin_settings.get("icons", {}).items()}
-
-    # Default the site icon using the site logo
-    jazzmin_settings["site_icon"] = jazzmin_settings["site_icon"] or jazzmin_settings["site_logo"]
-
-    # Default the login logo using the site logo
-    jazzmin_settings["login_logo"] = jazzmin_settings["login_logo"] or jazzmin_settings["site_logo"]
-
-    # Default the login logo dark using the login logo
-    jazzmin_settings["login_logo_dark"] = jazzmin_settings["login_logo_dark"] or jazzmin_settings["login_logo"]
-
-    # ensure all model names are lower cased
-    jazzmin_settings["changeform_format_overrides"] = {
-        x.lower(): y.lower() for x, y in jazzmin_settings.get("changeform_format_overrides", {}).items()
-    }
+    if not jazzmin_settings:
+        jazzmin_settings = JazzminSettings()
 
     return jazzmin_settings
 
 
-def get_ui_tweaks() -> Dict:
-    raw_tweaks = copy.deepcopy(DEFAULT_UI_TWEAKS)
-    raw_tweaks.update(getattr(settings, "JAZZMIN_UI_TWEAKS", {}))
-    tweaks = {x: y for x, y in raw_tweaks.items() if y not in (None, "", False)}
+def get_ui_tweaks() -> UITweaks:
+    tweaks: Union[UITweaks, dict[str, Any], None] = getattr(settings, "JAZZMIN_UI_TWEAKS")
+    if isinstance(tweaks, dict):
+        tweaks = UITweaks(**tweaks)
+
+    if not tweaks:
+        tweaks = UITweaks()
 
     # These options dont work well together
     if tweaks.get("layout_boxed"):
--- jazzmin/types.py
+++ jazzmin/types.py
@@ -1,4 +1,10 @@
 from enum import StrEnum
+from functools import cached_property
+from typing import Any, Dict, List, Optional
+
+from pydantic import BaseModel, Field
+
+from .utils import get_admin_url, get_model_meta
 
 
 class ChangeFormTemplate(StrEnum):
@@ -48,3 +54,170 @@ class Themes(StrEnum):
 
     def is_dark(self) -> bool:
         return self in DarkThemes.__members__.values()
+
+
+class SearchModel(BaseModel):
+    search_name: str
+    search_url: str
+
+
+class JazzminSettings(BaseModel):
+    site_title: Optional[str] = Field(
+        default=None, description="Title of the window (Will default to current_admin_site.site_title)"
+    )
+    site_header: Optional[str] = Field(
+        default=None,
+        description="Title on the login screen (19 chars max) (will default to current_admin_site.site_header)",
+    )
+    site_brand: Optional[str] = Field(
+        default=None, description="Title on the brand (19 chars max) (will default to current_admin_site.site_header)"
+    )
+    site_logo: str = Field(
+        default="vendor/adminlte/img/AdminLTELogo.png",
+        description="Relative path to logo for your site, used for brand on top left (must be present in static files)",
+    )
+    login_logo: Optional[str] = Field(
+        default=None,
+        description="Relative path to logo for your site, used for login logo (must be present in static files. Defaults to the same as site_logo",
+    )
+    login_logo_dark: Optional[str] = Field(
+        default=None,
+        description="Logo to use for login form in dark themes (must be present in static files. Defaults to login_logo)",
+    )
+    site_logo_classes: str = Field(default="img-circle", description="CSS classes that are applied to the logo")
+    site_icon: Optional[str] = Field(
+        default=None,
+        description="Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)",
+    )
+    welcome_sign: str = Field(default="Welcome", description="Welcome text on the login screen")
+    copyright: str = Field(default="", description="Copyright on the footer")
+    search_models: List[str] = Field(
+        default=[],
+        description="The models to search from the search bar, search bar omitted if excluded",
+        examples=["auth.User", "auth.Group"],
+    )
+    user_avatar: Optional[str] = Field(
+        default=None,
+        description="Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user",
+    )
+    topmenu_links: List[Any] = Field(default=[], description="Links to put along the nav bar")
+    usermenu_links: List[Any] = Field(
+        default=[],
+        description="Additional links to include in the user menu on the top right ('app' url type is not allowed)",
+    )
+    show_sidebar: bool = Field(default=True, description="Whether to display the side menu")
+    navigation_expanded: bool = Field(default=True, description="Whether to aut expand the menu")
+    hide_apps: List[str] = Field(
+        default=[], description="Hide these apps when generating side menu e.g (auth)", examples=["auth"]
+    )
+    hide_models: List[str] = Field(
+        default=[], description="Hide these models when generating side menu (e.g auth.user)", examples=["auth.user"]
+    )
+    order_with_respect_to: List[str] = Field(default=[], description="List of apps to base side menu ordering off of")
+    custom_links: Dict[str, Any] = Field(
+        default={},
+        description="Custom links to append to side menu app groups, keyed on lower case app label or makes a new group if the given app label doesnt exist in installed apps",
+    )
+
+    # TODO: ensure apps/models are the correct case too
+    # TODO: Ensure icon model names and classes are lower case
+    icons: Dict[str, str] = Field(
+        default={"auth": "fas fa-users-cog", "auth.user": "fas fa-user", "auth.Group": "fas fa-users"},
+        description="Custom icons for side menu apps/models",
+    )
+    default_icon_parents: str = Field(
+        default="fas fa-chevron-circle-right", description="Icons that are used when one is not manually specified"
+    )
+    default_icon_children: str = Field(
+        default="fas fa-circle", description="Icons that are used when one is not manually specified"
+    )
+    related_modal_active: bool = Field(default=False, description="Activate Bootstrap modal")
+    custom_css: Optional[str] = Field(
+        default=None, description="Relative paths to custom CSS/JS scripts (must be present in static files)"
+    )
+    custom_js: Optional[str] = Field(
+        default=None, description="Relative paths to custom CSS/JS scripts (must be present in static files)"
+    )
+    use_google_fonts_cdn: bool = Field(
+        default=True,
+        description="Whether to link font from fonts.googleapis.com (use custom_css to supply font otherwise)",
+    )
+    show_ui_builder: bool = Field(default=False, description="Whether to show the UI customizer on the sidebar")
+    changeform_format: str = Field(
+        default="horizontal_tabs", description="Render out the change view as a single form, or in tabs"
+    )
+    changeform_format_overrides: Dict[str, str] = Field(
+        default={}, description="Override change forms on a per modeladmin basis"
+    )
+    language_chooser: bool = Field(default=False, description="Add a language dropdown into the admin")
+
+    @cached_property
+    def search_models_parsed(self) -> List[SearchModel]:
+        search_models: List[SearchModel] = []
+
+        for search_model in self.search_models:
+            model_meta = get_model_meta(search_model)
+            search_url = get_admin_url(search_model)
+
+            if model_meta and model_meta.verbose_name_plural:
+                search_name = model_meta.verbose_name_plural.title()
+            else:
+                search_name = search_model.split(".")[-1] + "s"
+
+            search_models.append(SearchModel(search_name=search_name, search_url=search_url))
+
+        return search_models
+
+        # Default the login logo using the site logo
+
+    def get_login_logo(self) -> str:
+        return self.login_logo or self.site_logo
+
+    def get_login_logo_dark(self) -> str:
+        return self.login_logo_dark or self.login_logo or self.site_logo
+
+
+# DEFAULT_SETTINGS = JazzminSettings().dict()
+# DEFAULT_UI_TWEAKS = UITweaks().dict()
+
+
+class ButtonClasses(BaseModel):
+    primary: str = "btn-primary"
+    secondary: str = "btn-secondary"
+    info: str = "btn-info"
+    warning: str = "btn-warning"
+    danger: str = "btn-danger"
+    success: str = "btn-success"
+
+
+class UITweaks(BaseModel):
+    """
+    Currently available UI tweaks, Use the UI builder to generate this
+    """
+
+    navbar_small_text: bool = Field(False, description="Small text on the top navbar")
+    footer_small_text: bool = Field(False, description="Small text on the footer")
+    body_small_text: bool = Field(False, description="Small text everywhere")
+    brand_small_text: bool = Field(False, description="Small text on the brand/logo")
+    brand_colour: Optional[str] = Field(None, description="Brand/logo background colour")
+    accent: str = Field("accent-primary", description="Link colour")
+    navbar: str = Field("navbar-white navbar-light", description="Topmenu colour")
+    no_navbar_border: bool = Field(False, description="Topmenu border")
+    navbar_fixed: bool = Field(False, description="Make the top navbar sticky, keeping it in view as you scroll")
+    layout_boxed: bool = Field(
+        False, description="Whether to constrain the page to a box (leaving big margins at the side)"
+    )
+    footer_fixed: bool = Field(False, description="Make the footer sticky, keeping it in view all the time")
+    sidebar_fixed: bool = Field(False, description="Make the sidebar sticky, keeping it in view as you scroll")
+    sidebar: str = Field("sidebar-dark-primary", description="Sidemenu colour")
+    sidebar_nav_small_text: bool = Field(False, description="Sidemenu small text")
+    sidebar_disable_expand: bool = Field(False, description="Disable expanding on hover of collapsed sidebar")
+    sidebar_nav_child_indent: bool = Field(False, description="Indent child menu items on sidebar")
+    sidebar_nav_compact_style: bool = Field(False, description="Use a compact sidebar")
+    sidebar_nav_legacy_style: bool = Field(False, description="Use the AdminLTE2 style sidebar")
+    sidebar_nav_flat_style: bool = Field(False, description="Use a flat style sidebar")
+    theme: str = Field("default", description="Bootstrap theme to use (default, or from bootswatch)")
+    dark_mode_theme: Optional[str] = Field(None, description="Theme to use instead if the user has opted for dark mode")
+    button_classes: ButtonClasses = Field(
+        default_factory=ButtonClasses, description="The classes/styles to use with buttons"
+    )
