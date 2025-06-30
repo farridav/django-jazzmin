3874806de66b7cc52c1d37249198acd084d430b0
Didjer Perroud <didierperroud@stephanes-mini.home>
2025-06-30 15:28:26 +0200

    feat: implement dashboard model filter

--- jazzmin/static/jazzmin/js/dashboard_filter.js
+++ jazzmin/static/jazzmin/js/dashboard_filter.js
@@ -2,6 +2,22 @@
     function filterDashboardModels() {
         $("#model-filter-dashboard").on("input", function() {
             const filter = $(this).val().toLowerCase();
+
+            $("#card-container > * > .card").each(function() {
+                let appHasMatch = false;
+                const $appCard = $(this);
+                const $rows = $appCard.find("table > tbody > tr");
+
+                $rows.each(function() {
+                    const modelName = $(this).find("td:first-child > a").text().toLowerCase();
+                    const isMatch = modelName.includes(filter);
+
+                    $(this).toggle(isMatch);
+                    if (isMatch) appHasMatch = true;
+                });
+
+                $appCard.toggle(appHasMatch);
+            });
         });
     };
 
